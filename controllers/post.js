2a8884b61616648751c18355b8bdb841348728d8
daniellanorris <daniellanorris@ufl.edu>
2023-03-26 16:01:50 -0400

    all tests pass

---
+++ controllers/post.js
@@ -0,0 +1,129 @@
+const { Post, Tag } = require('../models');
+const { post } = require('../models/Comment');
+
+async function create(req, res, next) {
+    const { title, body, tags } = req.body
+    if (!title || !body) {
+      return res.status(400).send('need body and title, sorry')
+    }
+    const post = await Post.create({
+      title:title,
+      body:body,
+      tags:tags
+    });
+    res.status(200).json(post)
+}
+
+// TODO: create a new post
+// if there is no title or body, return a 400 status
+// omitting tags is OK
+// create a new post using title, body, and tags
+// return the new post as json and a 200 status
+
+// should render HTML
+async function get(req, res) {
+  try {
+    const slug = req.params.slug
+    const post = await Post.findOne({ slug }).populate('tags').lean();
+ 
+    // TODO: Find a single post
+    // find a single post by slug and populate 'tags'
+    // you will need to use .lean() or .toObject()
+    post.createdAt = new Date(post.createdAt).toLocaleString('en-US', {
+      month: '2-digit',
+      day: '2-digit',
+      year: 'numeric',
+    })
+    post.comments.map(comment => {
+      comment.createdAt = new Date(comment.createdAt).toLocaleString('en-US', {
+        month: '2-digit',
+        day: '2-digit',
+        year: 'numeric',
+      })
+      return comment
+    })
+    res.render('view-post', { post, isLoggedIn: req.session.isLoggedIn })
+
+  } catch (err) {
+    res.status(500).send(err.message)
+  }
+}
+
+// should render HTML
+async function getAll(req, res) {
+  try {
+    // get by single tag id if included
+    const mongoQuery = {}
+    if (req.query.tag) {
+      const tagDoc = await Tag.findOne({ name: req.query.tag }).lean()
+      if (tagDoc)
+        mongoQuery.tags = { _id: tagDoc._id }
+    }
+    const postDocs = await Post
+      .find(mongoQuery)
+      .populate({
+        path: 'tags'
+      })
+      .sort({ createdAt: 'DESC' })
+    const posts = postDocs.map(post => {
+      post = post.toObject()
+      post.createdAt = new Date(post.createdAt).toLocaleString('en-US', {
+        weekday: 'short',
+        month: 'short',
+        day: 'numeric',
+        year: 'numeric',
+      })
+      return post
+    })
+    res.render('index', {
+      posts,
+      isLoggedIn: req.session.isLoggedIn,
+      tag: req.query.tag
+    })
+  } catch (err) {
+    res.status(500).send(err.message)
+  }
+}
+
+async function update(req, res) {
+  try {
+    const { title, body, tags } = req.body
+    const postId = req.params.id
+    if(!title || !body) {
+      return res.status(400).send('need body and title, sorry')
+    }
+    // TODO: update a post
+    // if there is no title or body, return a 400 status
+    // omitting tags is OK
+  const updatedPost = await Post.updateOne(
+     {_id: postId},
+     {$set: {title:title, body:body, tags:tags}}
+   );
+  
+    return res.status(200).send('updated the post').json(updatedPost)
+    // find and update the post with the title, body, and tags
+    // return the updated post as json
+  } catch (err) {
+    res.status(500).send(err.message)
+  }
+
+}
+
+async function remove(req, res, next) {
+  const postId = req.params.id
+  try {
+    // TODO: Delete a post
+    // delete post by id, return a 200 status
+    await Post.findByIdAndDelete(postId);
+    return res.status(200).send('post successfully deleted')
+  } catch (err) {
+    res.status(500).send(err.message)
+  }
+}
+module.exports = {
+  get,
+  getAll,
+  create,
+  update,
+  remove
+}
---
+++ models/Tag.js
@@ -0,0 +1,12 @@
+const { Schema, model, models } = require('mongoose');
+
+const TagSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    }
+// Create a name property with type String and make it required and unique
+})
+
+module.exports = models.Tag || model('Tag', TagSchema)
